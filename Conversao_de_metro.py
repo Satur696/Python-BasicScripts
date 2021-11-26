73925e855e8a1ca31989a0bd3d0f2103492e511c
Satur <82034427+Satur696@users.noreply.github.com>
2021-11-26 11:54:55 -0300

    Add files via upload

---
+++ Conversao_de_metro.py
@@ -0,0 +1,7 @@
+metro = float(input("\nMetro: "))
+km = metro/1000
+print("Quilômetros: ", km, "\n")
+cent = metro*100
+print("Centímetro: ", cent, "\n")
+mili = metro*1000
+print("Milímetro: ", mili)
