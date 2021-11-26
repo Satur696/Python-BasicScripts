42d726c72ca44934e24f926bee82f4c9b4842edc
Satur <82034427+Satur696@users.noreply.github.com>
2021-11-26 11:55:48 -0300

    Add files via upload

---
+++ Media_Aritmetica.py
@@ -0,0 +1,20 @@
+l1 = float(input("L1:   "))
+l2 = float(input("\nL2:   "))
+n1 = l1+l2
+print("\nNota 1:  ", n1)
+p1 = float(input("\nP1:   "))
+l3 = float(input("\nL3:   "))
+l4 = float(input("\nL4:   "))
+n2 = p1+l3+l4 
+print("\nNota 2:  ", n2)
+p2 = float(input("\nP2:   "))
+l5 = float(input("\nL5:   "))
+n3 = p2+l5
+print("\nNota 3:  ", n3)
+p3 = float(input("\nP2:   "))
+l6 = float(input("\nL6:   "))
+l7 = float(input("\nL7:   "))
+n4 = p3+l6+l7
+print("\nNota 4:  ", n4)
+media = (n1+n2+n3+n4)/4
+print("A média é:   ", media)
