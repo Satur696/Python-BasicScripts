fde74fb26ccf4b1298c17671be75a9b743b8c344
Satur <82034427+Satur696@users.noreply.github.com>
2021-12-10 20:10:35 -0300

    Update Desafio.py

--- Desafio.py
+++ Desafio.py
@@ -3,20 +3,29 @@ print("\t\t    Desafio - Q7\n")
 
 try:
     inteiro = int(input("Digite o valor de i:"))
+    if inteiro<0:
+        print("O valor de i não pode ser menor que 0")
+        exit("Saindo...")
+    
 except:
-    print("Erro no Sistema!")
+    print("Erro no Sistema!\nMotivo: O valor de 'i' não pode ser uma String")
+    exit("Saindo...")
+
 try:
     a = float(input("Digite o valor de a:"))
 except:
-    print("Erro no Sistema!")
+    print("Erro no Sistema!\nMotivo: O valor de 'a' não pode ser uma String")
+    exit("Saindo...")
 try:
     b  = float(input("Digite o valor de b:"))
 except:
-    print("Erro no Sistema!")
+    print("Erro no Sistema!\nMotivo: O valor de 'b' não pode ser uma String")
+    exit("Saindo...")
 try:
     c = float(input("Digite o valor de c:"))
 except:
-    print("Erro no Sistema!")
+    print("Erro no Sistema!\nMotivo: O valor de 'c' não pode ser uma String")
+    exit("\nSaindo...")
 
 if inteiro==2:
     calculo = (a+b+c)/3
