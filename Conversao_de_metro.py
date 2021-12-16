672e38080bbff3a8f7b95e38fa7b88675e6945a7
Satur <82034427+Satur696@users.noreply.github.com>
2021-12-16 19:56:43 -0300

    Delete Conversao_de_metro.py

--- Conversao_de_metro.py
+++
@@ -1,7 +0,0 @@
-metro = float(input("\nMetro: "))
-km = metro/1000
-print("Quilômetros: ", km, "\n")
-cent = metro*100
-print("Centímetro: ", cent, "\n")
-mili = metro*1000
-print("Milímetro: ", mili)
